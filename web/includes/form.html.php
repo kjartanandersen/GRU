2e4aa1541c1eb30f1dda4d480147abf705aee4bf
Kjartan Már Andersen <svidakjammi@gmail.com>
2015-04-28 09:22:55 +0000

    Create form.html.php
    form fyrir nýskráningarsíðuna.
    Kjartan Már Andersen

---
+++ web/includes/form.html.php
@@ -0,0 +1,36 @@
+
+<div class="container-fluid">
+<div class="container">
+<form form role="form" action="?" method="post" id="validationform">
+
+<div class="containerskra1">
+<div id="widthnys">
+<div id="nuverny">
+	<h2 class="innstxt">Núna verður þú að nýskrá þig</h2>
+    <p id="nuverdmarg" class="innstxt">Nauðsynlegt að fylla út í allt</p>
+</div>
+         <div class="nyskrainput">
+         <div class="input-group">
+         	<span class="input-group-addon" id="basic-addon1">Fullt Nafn</span>
+         	<input type="fullname" name="fullname" class="form-control" id="fixinput" placeholder="Fullt Nafn" aria-describedby="basic-addon1" required>
+         </div>
+         <div class="input-group">
+         	<span class="input-group-addon" id="basic-addon1">Notendanafn</span>
+            <input type="username" name="username" class="form-control" id="fixinput" placeholder="Notendanafn" aria-describedby="basic-addon1" required>
+        </div>
+        <div class="input-group">
+        	<span class="input-group-addon" id="basic-addon1">Password</span>
+            <input type="password" name="password" class="form-control" id="fixinput" placeholder="Password" aria-describedby="basic-addon1" required>
+        </div>
+        </div>
+
+	<div class="nyskrainnput">
+		<input type="submit" value="Skrá" class="btn btn-default">
+        <a class="btn btn-default" href="../index.php" role="button">Til Baka</a>
+	</div>
+</div>
+</div>
+</form>
+</div>
+</div>
+
