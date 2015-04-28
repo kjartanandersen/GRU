41c5b89c8545f0c0097f1e824661c719e9fd4cbc
Kjartan Már Andersen <svidakjammi@gmail.com>
2015-04-28 09:22:13 +0000

    Create index.php
    nýskráningarsíðan, index
    Kjartan Már Andersen

---
+++ web/includes/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>GRU Forsíða</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="../css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="../css/custon.css" rel="stylesheet" type="text/css">
+    <script src="../js/respond.min.js" type="text/javascript"></script>
+    <script src="../js/bootstrap.js" type="text/javascript"></script>
+    
+</head>
+<body>
+<?php
+	session_start();
+	include 'form.html.php';
+	include "dbcon.php";		
+	include "CreateNotendur.php";
+?>
+</body>
