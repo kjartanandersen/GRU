1dc481a72593a2780cf665fac070f09c25e9a813
Kjartan Már Andersen <svidakjammi@gmail.com>
2015-05-06 16:22:23 +0000

    Create takk.php
    þakkar fyrir skráningu

---
+++ web/includes/takk.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+	<html>
+	    <head>
+	        <meta charset="UTF-8">
+	        <title>Skráning lokið</title>
+	    </head>
+	    
+	    <body>
+	    	<h1>Takk fyrir skráninguna</h1>
+		</body>
+	</html>
